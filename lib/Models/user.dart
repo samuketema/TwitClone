8a776875ad2083357624a8b26043fc7ee241d9c2
Samuel <samuelketema01@gmail.com>
2024-09-27 09:26:40 +0300

    user data class.

---
+++ lib/Models/user.dart
@@ -0,0 +1,47 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+class FirebaseUser {
+  String email;
+  FirebaseUser({
+    required this.email,
+  });
+  
+
+  FirebaseUser copyWith({
+    String? email,
+  }) {
+    return FirebaseUser(
+      email: email ?? this.email,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'email': email,
+    };
+  }
+
+  factory FirebaseUser.fromMap(Map<String, dynamic> map) {
+    return FirebaseUser(
+      email: map['email'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory FirebaseUser.fromJson(String source) => FirebaseUser.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() => 'FirebaseUser(email: $email)';
+
+  @override
+  bool operator ==(covariant FirebaseUser other) {
+    if (identical(this, other)) return true;
+  
+    return 
+      other.email == email;
+  }
+
+  @override
+  int get hashCode => email.hashCode;
+}
