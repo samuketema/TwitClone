3f6273560cb31da80d0e46861cee25ec2e5bfd13
Samuel <samuelketema01@gmail.com>
2024-09-27 09:23:21 +0300

    wrapping up with Provider scope

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:twitter/firebase_options.dart';
+import 'package:twitter/pages/home.dart';
 import 'pages/signin_page.dart';
 
-void main() => runApp( MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+ await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp( ProviderScope(child: MyApp()));
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -10,7 +19,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Twitter App',
-      home: SignInPage(),
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return HomePage();
+          }
+          return SignInPage();
+        }
+      ),
     );
   }
 }
