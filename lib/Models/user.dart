20813fb2088eff75dee0941b9233e5fd56b17bf1
Samuel <samuelketema01@gmail.com>
2024-09-28 22:19:23 +0300

    Name and profiepic added.

--- lib/Models/user.dart
+++ lib/Models/user.dart
@@ -1,29 +1,44 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
+
+
 class FirebaseUser {
   String email;
+  String name;
+  String proofilePic;
+  
   FirebaseUser({
     required this.email,
+    required this.name,
+    required this.proofilePic,
   });
   
 
   FirebaseUser copyWith({
     String? email,
+    String? name,
+    String? proofilePic,
   }) {
     return FirebaseUser(
       email: email ?? this.email,
+      name: name ?? this.name,
+      proofilePic: proofilePic ?? this.proofilePic,
     );
   }
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
       'email': email,
+      'name': name,
+      'proofilePic': proofilePic,
     };
   }
 
   factory FirebaseUser.fromMap(Map<String, dynamic> map) {
     return FirebaseUser(
       email: map['email'] as String,
+      name: map['name'] as String,
+      proofilePic: map['proofilePic'] as String,
     );
   }
 
@@ -32,16 +47,18 @@ class FirebaseUser {
   factory FirebaseUser.fromJson(String source) => FirebaseUser.fromMap(json.decode(source) as Map<String, dynamic>);
 
   @override
-  String toString() => 'FirebaseUser(email: $email)';
+  String toString() => 'FirebaseUser(email: $email, name: $name, proofilePic: $proofilePic)';
 
   @override
   bool operator ==(covariant FirebaseUser other) {
     if (identical(this, other)) return true;
   
     return 
-      other.email == email;
+      other.email == email &&
+      other.name == name &&
+      other.proofilePic == proofilePic;
   }
 
   @override
-  int get hashCode => email.hashCode;
+  int get hashCode => email.hashCode ^ name.hashCode ^ proofilePic.hashCode;
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,22 +1,64 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:twitter/pages/settings.dart';
 import 'package:twitter/providers/user_provider.dart';
 
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context , WidgetRef ref) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    LocalUser currentUser = ref.watch(userProvider);
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        leading: Builder(builder: (context) {
+          return GestureDetector(
+            onTap: () => Scaffold.of(context).openDrawer(),
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: CircleAvatar(
+                backgroundImage: NetworkImage(currentUser.user.proofilePic),
+              ),
+            ),
+          );
+        }),
         title: Text('Home Page'),
-        actions: [TextButton(onPressed: () {
-          FirebaseAuth.instance.signOut();
-          ref.read(userProvider.notifier).logout();
-        }, child: Text('Sign out'))],
       ),
-      body: Center(child: Text(ref.watch(userProvider).user.email),),
+      body: Column(children: [
+        Text(currentUser.user.name),
+        Text(currentUser.user.email),
+      ]),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            Image.network(currentUser.user.proofilePic),
+            ListTile(
+              title: Text(
+                "Welcome ${currentUser.user.name}",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+            ),
+            ListTile(
+                title: Text('settings'),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) => Setting()));
+                }),
+            ListTile(
+              title: Text('Sign out'),
+              onTap: () {
+                FirebaseAuth.instance.signOut();
+                ref.read(userProvider.notifier).logout();
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
